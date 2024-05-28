c0aad722216596a3c50806bc3e0f131fa85554c8
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 11:40:00 +0900

    Feat : 플레이어 유닛들 스폰매니저 기능 구현
    플레이어 유닛들 스폰매니저 기능 구현

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/Manager/PlayerSpawnManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSpawnManager : MonoBehaviour
+{
+    [Header("플레이어 생성 주기")]
+    [SerializeField]
+    private float respawn_P_Cycle;
+
+    [Header("플레이어 유닛들")]
+    [SerializeField]
+    private UnitData[] playerUnits;
+
+    [Header("플레이어 현재 주기")]
+    [SerializeField]
+    private float[] current_Cycle;
+
+    [Header("플레이어 현재 주기")]
+    [SerializeField]
+    private Image[] unitsImgs;
+
+    [Header("모든 플레이어 죽었는지 확인하는 변수")]
+    [SerializeField]
+    public bool isPlayer_All_Dead;
+
+    [Header("스테이지 매니저 스크립트")]
+    [SerializeField]
+    private StageManager stageManagerCs;
+
+
+    private void Update()
+    {
+        // 플레이어가 다 죽지 않았을 때만 리스폰 함수 호출
+        if(!isPlayer_All_Dead)
+        {
+            //플레이어가 모두 죽었는지 확인하는 변수
+            playerDeadCheck();
+
+            for (int i = 0; i < playerUnits.Length; i++)
+            {
+                if (!playerUnits[i].gameObject.activeSelf)
+                    RespawnPlayer(i);
+            }
+        }
+    }
+
+    // 사망한 플레이어 유닛 생성 주기 이후 재생성해주는 함수
+    public void RespawnPlayer(int playerUnit_Num)
+    {
+        if (respawn_P_Cycle > current_Cycle[playerUnit_Num])
+        {
+            current_Cycle[playerUnit_Num] += Time.deltaTime;
+
+            unitsImgs[playerUnit_Num].fillAmount = current_Cycle[playerUnit_Num] / respawn_P_Cycle;
+        }
+
+        else if (respawn_P_Cycle <= current_Cycle[playerUnit_Num])
+            SetInit(playerUnit_Num);
+    }
+    
+    //플레이어가 모두 죽었는지 확인하는 변수
+    private void playerDeadCheck()
+    {
+        // 플레이어 캐릭터들이 전부 죽으면 게임 끝
+        isPlayer_All_Dead = !playerUnits[0].gameObject.activeSelf && !playerUnits[1].gameObject.activeSelf && !playerUnits[2].gameObject.activeSelf && !playerUnits[3].gameObject.activeSelf;
+
+        // 게임 끝인 것을 전달
+        if (isPlayer_All_Dead)
+            stageManagerCs.gameEnd = true;
+    }
+
+    // 재생성될 플레이어 유닛 기초값 할당해주는 함수
+    private void SetInit(int playerUnit_Num)
+    {
+        playerUnits[playerUnit_Num].InitValue();
+
+        playerUnits[playerUnit_Num].transform.position = playerUnits[playerUnit_Num].base_Pos.position;
+
+        playerUnits[playerUnit_Num].gameObject.SetActive(true);
+
+        current_Cycle[playerUnit_Num] = 0f;
+
+    }
+
+
+}
