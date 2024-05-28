90f2ba23dfd10387bcf34f71df6b440b2fe0f450
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 10:36:43 +0900

    Feat : 유닛 스킬 공격 시 호출되는 애니메이션 이벤트 기능 구현
    유닛 스킬 공격 시 호출되는 애니메이션 이벤트 기능 구현

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/UnitAct/UnitAtk/UnitSkillAtk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSkillAtk : MonoBehaviour
+{
+    [SerializeField]
+    private UnitData unitDataCs;
+
+    [SerializeField]
+    private ISearchTarget targetDamagedCs;
+
+
+    [SerializeField]
+    private IUseSkill skillCs;
+
+    private void Awake()
+    {
+        unitDataCs = transform.parent.GetComponent<UnitData>();
+        targetDamagedCs = unitDataCs.transform.GetComponent<ISearchTarget>();
+        skillCs = transform.parent.GetComponent<IUseSkill>();
+        print(skillCs);
+    }
+
+    // 스킬을 사용할 때 호출되는 함수
+    public void UseSkill_Atk()
+    {
+        // 캐릭터 타입이 힐러가 아니고, 타겟이 없을 경우 탈출
+        if (!unitDataCs.characterType.Equals(CharacterType.Healer) && targetDamagedCs._targetUnit == null)
+            return;
+
+        // 이외의 경우 유닛 쿨타임 0으로 전환
+        unitDataCs._current_SkillCoolTime = 0f;
+
+        // 내가 갖고 있는 스킬 사용
+        skillCs.UseSkill();
+    }
+}
