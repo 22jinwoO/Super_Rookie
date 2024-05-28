213ef99c1100ff63b28675fdb4c92c4970e249fa
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 12:26:03 +0900

    Feat : 에셋 추가
    에셋 추가

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/Spell/Spell_Vfx.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell_Vfx : MonoBehaviour
+{
+	void OnEnable()
+	{
+		StartCoroutine("CheckIfAlive");
+	}
+	IEnumerator CheckIfAlive()
+	{
+
+		yield return new WaitForSeconds(1.5f);
+		gameObject.SetActive(false);
+	}
+
+}
---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/UnitAct/Player/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("플레이어 골드 획득량")]
+    public int goldValue;
+
+}
