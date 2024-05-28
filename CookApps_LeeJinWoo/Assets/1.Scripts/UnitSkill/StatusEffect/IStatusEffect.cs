5c5670ca6c661c9f4c92e3d05769f470929d7a23
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 10:37:42 +0900

    Feat : 상태이상 효과 인터페이스 구현
    상태이상 효과 인터페이스 구현

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/UnitSkill/StatusEffect/IStatusEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IStatusEffect
+{
+    // 상태이상 효과 이펙트
+    public GameObject _skillVfx { get; set; }
+
+    // 상태 이상 지속 시간
+    public float _statusEffecttime { get; set; }
+
+    // 상태이상 적용하는 함수 정의
+
+    public void OnStatusEffect(float StatusEffecttime);
+}
