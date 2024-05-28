d3f39fc31d5e22008c240e8296102d24a456e85f
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 11:14:53 +0900

    Feat : 몬스터 팩토리 인터페이스  구현
    몬스터 팩토리 인터페이스  구현

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/Interface/FactoryInterface/IMonsterFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMonsterFactory
+{
+    // 몬스터 프리팹
+    public UnitData _monsterPrefab { get; set; }
+
+    // 오브젝트 풀링을 위한 몬스터 유닛 풀
+    public Stack<UnitData> _monsters { get; set; }
+
+    // InitObjPool() : 오브젝트 풀링 초기값 셋팅하는 함수
+    public void InitObjPool(UnitData monsterPref);
+
+
+    // 몬스터 유닛 생산하는 생성자 함수
+    public UnitData CreateMonsterUnit();
+
+}
