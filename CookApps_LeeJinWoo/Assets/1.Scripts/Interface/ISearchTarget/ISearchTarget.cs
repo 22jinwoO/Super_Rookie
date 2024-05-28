56d2228a3caab40db004a6ad46e25a0f4a665392
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 10:25:15 +0900

    Feat : 적 탐지 시 필요한 인터페이스 및 추상 클래스 기능 구현
    적 탐지 시 필요한 인터페이스 및 추상 클래스 기능 구현

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/Interface/ISearchTarget/ISearchTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISearchTarget
+{
+    // 유닛이 타겟으로 할 대상
+    public Transform _targetUnit { get; set; }
+
+    public CapsuleCollider2D _targetColider { get; set; }
+
+    // 유닛의 행동 상태
+    public IUnitActState _actStateCs { get; set; }
+
+    // 타겟을 탐지하는 함수 선언
+    public void SearchTarget();
+}
---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/SearchTarget/AbsSearchTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbsSearchTarget : MonoBehaviour
+{
+    [SerializeField]
+    protected UnitData unitDataCs;
+
+    [SerializeField]
+    protected LayerMask layerMask = 0;   // 오버랩스피어 레이어 마스크 변수
+
+}
