7698a9235ae3a12c20721bed3abdccb497024ebb
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 10:50:41 +0900

    Feat : 스킬 인터페이스 구현
    스킬 인터페이스 구현

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/Interface/Skill_Interface/IUseSkill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUseSkill
+{
+    // 스킬 이펙트 게임오브젝트
+    public GameObject _skillVFx { get; set; }
+
+    // 스킬 사용 시 호출되는 함수
+    public void UseSkill();
+}
