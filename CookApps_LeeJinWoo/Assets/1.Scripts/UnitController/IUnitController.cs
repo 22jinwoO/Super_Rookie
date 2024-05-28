6849fba63ca67f48241feadb8e5aa848723a2362
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 10:30:40 +0900

    Feat : 유닛 컨트롤러 인터페이스 구현
    여러 종류의 유닛들이 공통적으로 가지는 컨트롤러 데이터 추상화

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/UnitController/IUnitController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUnitController
+{
+    public Rigidbody2D _rigid { get; set; }
+
+    // 유닛 동작 타입
+    public UnitAction actionState { get; set; }
+
+    // 클래스들의 접근점(인터페이스)
+    public IUnitActState _unitState { get; set; }
+
+    // 유닛들 동작 시 호출되는 함수
+    public void setActionType(UnitAction state);
+}
