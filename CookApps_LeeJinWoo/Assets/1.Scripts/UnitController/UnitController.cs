2078f9ea14fbb8238896f5c95c1680635d0351e9
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 10:31:12 +0900

    Feat : 프로토타입 유닛 컨트롤러 기능 구현
    프로토타입 유닛 컨트롤러 기능 구현

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/UnitController/UnitController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    public UnitAction actionState;
+
+    // Concrete클래스들의 접근점(인터페이스)
+    public IUnitActState _unitState;
+
+    [Header("리지드 바디")]
+    public Rigidbody2D _rigid;
+
+}
