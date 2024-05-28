229ca2b0fc1b874bdbe33af14196b5e5cc2fbd83
Lee Jin Woo <jinwoo2511@naver.com>
2024-05-28 10:24:11 +0900

    Feat : 유닛 행동 인터페이스 구현
    유닛 행동 인터페이스 구현

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/Interface/IUnitState/IUnitState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUnitActState
+{
+    // 행동 진입 시 호출
+    public void Enter();
+
+    // 행동 중 호출
+    public void DoAction();
+
+    // 행동 그만둘 때 호출
+    public void Exit();
+}
+
