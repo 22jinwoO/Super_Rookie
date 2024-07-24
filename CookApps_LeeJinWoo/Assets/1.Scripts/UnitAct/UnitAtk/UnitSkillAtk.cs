d377e0d3a2da6d40baf7cf22a5c41c6f2f284700
22jinWoO <jinwoo2511@naver.com>
2024-07-25 03:23:14 +0900

    refac : 변수 자료형 및 네이밍 수정
    변수 자료형 및 네이밍 수정

--- CookApps_LeeJinWoo/Assets/1.Scripts/UnitAct/UnitAtk/UnitSkillAtk.cs
+++ CookApps_LeeJinWoo/Assets/1.Scripts/UnitAct/UnitAtk/UnitSkillAtk.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class UnitSkillAtk : MonoBehaviour
 {
     [SerializeField]
-    private UnitData unitDataCs;
+    private BaseUnitData unitData;
 
     [SerializeField]
-    private ISearchTarget targetDamagedCs;
+    private ISearchTarget targetDamaged;
 
 
     [SerializeField]
@@ -16,8 +16,8 @@ public class UnitSkillAtk : MonoBehaviour
 
     private void Awake()
     {
-        unitDataCs = transform.parent.GetComponent<UnitData>();
-        targetDamagedCs = unitDataCs.transform.GetComponent<ISearchTarget>();
+        unitData = transform.parent.GetComponent<BaseUnitData>();
+        targetDamaged = unitData.transform.GetComponent<ISearchTarget>();
         skillCs = transform.parent.GetComponent<IUseSkill>();
         print(skillCs);
     }
@@ -26,11 +26,11 @@ public class UnitSkillAtk : MonoBehaviour
     public void UseSkill_Atk()
     {
         // 캐릭터 타입이 힐러가 아니고, 타겟이 없을 경우 탈출
-        if (!unitDataCs.characterType.Equals(CharacterType.Healer) && targetDamagedCs._targetUnit == null)
+        if (!unitData.CharacterType.Equals(CharacterType.Healer) && targetDamaged.TargetUnit == null)
             return;
 
         // 이외의 경우 유닛 쿨타임 0으로 전환
-        unitDataCs._current_SkillCoolTime = 0f;
+        unitData.Current_SkillCoolTime = 0f;
 
         // 내가 갖고 있는 스킬 사용
         skillCs.UseSkill();
