933722bb4d1546194fbf6c886c2fee3be76b15af
22jinWoO <jinwoo2511@naver.com>
2024-07-25 03:22:32 +0900

    refac : 변수 자료형 및 네이밍 수정
    변수 자료형 및 네이밍 수정

--- CookApps_LeeJinWoo/Assets/1.Scripts/UnitAct/UnitTracking.cs
+++ CookApps_LeeJinWoo/Assets/1.Scripts/UnitAct/UnitTracking.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class UnitTracking : MonoBehaviour, IUnitActState
 {
     [SerializeField]
-    private UnitData unitDataCs;
+    private BaseUnitData unitData;
 
     [SerializeField]
     private IUnitController contorllerCs;
@@ -23,82 +23,82 @@ public class UnitTracking : MonoBehaviour, IUnitActState
 
     private void Awake()
     {
-        unitDataCs = GetComponent<UnitData>();
+        unitData = GetComponent<BaseUnitData>();
         contorllerCs = GetComponent<IUnitController>();
         searchTargetCs = GetComponent<ISearchTarget>();
         anim = GetComponentInChildren<Animator>();
     }
     public void Enter()
     {
-        contorllerCs._rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
+        contorllerCs.Rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     public void DoAction()
     {
         // 타겟이 없으면 탈출
-        if (searchTargetCs._targetUnit == null)
+        if (searchTargetCs.TargetUnit == null)
         {
             Exit();
             return;
         }
 
         // 리지드바디 각도 고정
-        contorllerCs._rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
+        contorllerCs.Rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
 
         // 방향전환
-        if (searchTargetCs._targetUnit.position.x > transform.position.x)
+        if (searchTargetCs.TargetUnit.position.x > transform.position.x)
             transform.localScale = new Vector3(-1f, 1f, 1f);
         else
             transform.localScale = Vector3.one;
 
         // 타겟을 향한 이동방향 구하기
-        Vector2 dir = (Vector2)searchTargetCs._targetUnit.position - (Vector2)transform.position;
+        Vector2 dir = (Vector2)searchTargetCs.TargetUnit.position - (Vector2)transform.position;
 
-        Vector2 nextVec = dir.normalized * unitDataCs._unit_Speed * Time.deltaTime;
+        Vector2 nextVec = dir.normalized * unitData.UnitSpeed * Time.deltaTime;
 
         // 타겟과 유닛과의 거리 구하기
-        float distance = Vector2.Distance((Vector2)searchTargetCs._targetUnit.position, (Vector2)transform.position);
+        float distance = Vector2.Distance((Vector2)searchTargetCs.TargetUnit.position, (Vector2)transform.position);
 
         // 플레이어 유닛이고, 타겟과의 거리가 스킬 사거리보다 클 경우 타겟을 향해 이동
-        if ((!gameObject.CompareTag("Monster") && distance > unitDataCs.unit_SkillRange))
+        if ((!gameObject.CompareTag("Monster") && distance > unitData.UnitSkillRange))
         {
             anim.SetBool(hashWalk, true);
-            contorllerCs._rigid.MovePosition(contorllerCs._rigid.position + nextVec);
+            contorllerCs.Rigid.MovePosition(contorllerCs.Rigid.position + nextVec);
         }
 
         // 유닛이 플레이어 캐릭터이고 이동중 타겟과의 거리가 스킬 사거리와 같거나 작아질 경우 멈추고 스킬 사용하기
-        else if (!gameObject.CompareTag("Monster") && distance <= unitDataCs.unit_SkillRange && unitDataCs._useSkill)
+        else if (!gameObject.CompareTag("Monster") && distance <= unitData.UnitSkillRange && unitData.UseSkill)
         {
             anim.SetBool(hashWalk, false);
 
-            contorllerCs._rigid.MovePosition(transform.position);
+            contorllerCs.Rigid.MovePosition(transform.position);
 
             Exit();
         }
 
         //타겟과의 거리가 공격사거리보다 클 경우 걷기 애니메이션 재실행
-        else if (distance > unitDataCs._unit_AtkRange)
+        else if (distance > unitData.AtkRange)
         {
-            contorllerCs._rigid.MovePosition(contorllerCs._rigid.position + nextVec);
+            contorllerCs.Rigid.MovePosition(contorllerCs.Rigid.position + nextVec);
             anim.SetBool(hashWalk, true);
         }
 
 
         // 타겟과의 거리가 공격사거리와 같거나 작아질 경우 멈추고 기본 공격하기
-        else if (distance <= unitDataCs._unit_AtkRange)
+        else if (distance <= unitData.AtkRange)
         {
             anim.SetBool(hashWalk, false);
-            contorllerCs._rigid.MovePosition(transform.position);
+            contorllerCs.Rigid.MovePosition(transform.position);
 
-            if (unitDataCs._canAtk)
+            if (unitData.CanAct)
                 Exit();
         }
 
         // 사거리 밖으로 타겟이 나가면 Idle 상태로 전환
-        if (distance > unitDataCs._unit_sightRange)
+        if (distance > unitData.UnitSightRange)
         {
-            unitDataCs._isSearch = false;
-            searchTargetCs._targetUnit = null;
+            unitData.SearchTarget.IsSearch = false;
+            searchTargetCs.TargetUnit = null;
             Exit();
             return;
         }
@@ -108,24 +108,24 @@ public class UnitTracking : MonoBehaviour, IUnitActState
     public void Exit()
     {
         // 타겟이 스킬 공격 범위 안에 있는 상태일 경우
-        if (!gameObject.CompareTag("Monster") && searchTargetCs._targetUnit != null && unitDataCs._useSkill)
+        if (!gameObject.CompareTag("Monster") && searchTargetCs.TargetUnit != null && unitData.UseSkill)
         {
-            contorllerCs._unitState = null;
-            contorllerCs.actionState = UnitAction.UseSkill;
+            contorllerCs.UnitState = null;
+            contorllerCs.Action = UnitAction.UseSkill;
         }
 
         // 타겟이 공격범위 안에 있는 상태일 경우
-        else if (searchTargetCs._targetUnit != null && unitDataCs._canAtk)
+        else if (searchTargetCs.TargetUnit != null && unitData.CanAct)
         {
-            contorllerCs._unitState = null;
-            contorllerCs.actionState = UnitAction.Attack;
+            contorllerCs.UnitState = null;
+            contorllerCs.Action = UnitAction.Attack;
         }
 
         // 타겟이 시야 범위 밖으로 나가서 타겟이 null 상태가 됐을 경우
-        else if (searchTargetCs._targetUnit == null)
+        else if (searchTargetCs.TargetUnit == null)
         {
-            contorllerCs._unitState = null;
-            contorllerCs.actionState = UnitAction.Idle;
+            contorllerCs.UnitState = null;
+            contorllerCs.Action = UnitAction.Idle;
         }
     }
 }
