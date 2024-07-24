8cd88e661171b49f09bcc5ef1915510564d18e17
22jinWoO <jinwoo2511@naver.com>
2024-07-25 03:10:16 +0900

    refac : BaseUnitData 클래스를 상속받은 몬스터 유닛 데이터 클래스 구현
    BaseUnitData 클래스를 상속받은 몬스터 유닛 데이터 클래스 구현
    스폰 위치 값, 몬스터 팩토리, 드랍골드, 드랍 경험치 속성 추가

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/UnitData/Monster/MonsterUnitData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterUnitData : BaseUnitData
+{
+    // 스폰 위치값
+    public Vector2 Default_Pos { get; set; }
+
+    // 몬스터 팩토리
+    public IMonsterFactory Factory { get; set; }
+
+    // 드랍 골드
+    [field : SerializeField]
+    public int DropGoldValue { get; set; }
+
+    // 드랍 경험치
+    [field: SerializeField]
+    public float DeathExpValue { get; set; }
+}
