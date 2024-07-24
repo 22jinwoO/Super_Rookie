0bde4280a559967024d1d1b85791f438d3e03fc6
22jinWoO <jinwoo2511@naver.com>
2024-07-25 03:32:23 +0900

    refac : 변수 자료형 및 변수들 네이밍 수정, 전달 받는 플레이어 유닛 스탯 값 수정
    변수 자료형 및 변수들 네이밍 수정, 전달 받는 플레이어 유닛 스탯 값 수정

--- CookApps_LeeJinWoo/Assets/1.Scripts/Manager/StatsManager.cs
+++ CookApps_LeeJinWoo/Assets/1.Scripts/Manager/StatsManager.cs
@@ -6,28 +6,28 @@ using TMPro;
 public class StatsManager : MonoBehaviour
 {
     [Header("스탯을 증가시킬 유닛")]
-    public UnitData _thisUnit;
+    public PlayerUnitData _thisUnit;
 
     //TextMeshProUGUI plusValueText,
     #region # GetStats() : 능력치를 증가시키는 해당 플레이어 캐릭터에 값 전달 어떤 능력치를 얼마만큼 증가시키는지 값 이벤트 전달, 후 다시 뷰에 증가된 데이터 전달
-    public void GetStats(UnitData playerUnit, PlusStas stat, float plusValue, TextMeshProUGUI toTalValueText, TextMeshProUGUI defaultValueText ,TextMeshProUGUI plusValueText)
+    public void GetStats(PlayerUnitData playerUnit, PlusStats stat, float plusValue, TextMeshProUGUI toTalValueText, TextMeshProUGUI defaultValueText ,TextMeshProUGUI plusValueText)
     {
         _thisUnit = playerUnit;
 
-        _thisUnit.Plus_PlayerStat(stat : stat, plusValue : plusValue);
+        _thisUnit.PlusPlayerStat(stat : stat, plusValue : plusValue);
 
         switch (stat)
         {
-            case PlusStas.Attack:
-                toTalValueText.text = $"{_thisUnit._unit_AtkDmg} ";
-                defaultValueText.text = $"{_thisUnit.default_AtkDmg + _thisUnit._level_Plus_AtkStat}";
-                plusValueText.text = $"{_thisUnit._plus_AtkStat}";
+            case PlusStats.Attack:
+                toTalValueText.text = $"{_thisUnit.AtkDmg} ";
+                defaultValueText.text = $"{_thisUnit.AtkDmg + _thisUnit.Plus_AtkStat}";
+                plusValueText.text = $"{_thisUnit.Plus_AtkStat}";
                 break;
 
-            case PlusStas.Hp:
-                toTalValueText.text = $"{_thisUnit._max_Hp} ";
-                defaultValueText.text = $"{_thisUnit.default_HpValue + _thisUnit._level_Plus_HpStat}";
-                plusValueText.text = $"{_thisUnit._plus_HpStat}";
+            case PlusStats.Hp:
+                toTalValueText.text = $"{_thisUnit.Max_Hp} ";
+                defaultValueText.text = $"{_thisUnit.Unit_Hp + _thisUnit.Plus_HpStat}";
+                plusValueText.text = $"{_thisUnit.Plus_HpStat}";
                 break;
         }
     }
