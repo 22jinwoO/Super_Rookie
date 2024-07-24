1b9d656ccdba3b7e557e83d4ef25437949d7ab2c
22jinWoO <jinwoo2511@naver.com>
2024-07-25 03:22:14 +0900

    refac : UTF - 8 로 인코딩하여 저장
    UTF - 8 로 인코딩하여 저장

---
+++ CookApps_LeeJinWoo/Assets/1.Scripts/UnitAct/UnitIdle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitIdle : MonoBehaviour, IUnitActState
+{
+
+    [SerializeField]
+    private IUnitController contorllerCs;
+
+    [SerializeField]
+    private ISearchTarget searchTargetCs;
+
+
+    public void Enter()
+    {
+        contorllerCs = GetComponent<IUnitController>();
+        searchTargetCs = GetComponent<ISearchTarget>();
+        searchTargetCs.ActState = this;
+    }
+
+    public void DoAction()
+    {
+        searchTargetCs.SearchTarget();
+    }
+
+    public void Exit()
+    {
+        contorllerCs.UnitState = null;
+        contorllerCs.Action = UnitAction.Tracking;
+    }
+
+}
