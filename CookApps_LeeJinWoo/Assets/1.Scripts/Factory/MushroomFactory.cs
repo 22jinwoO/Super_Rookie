2a6fc6ec42a23dfc4d57345e999f7a2854679575
22jinWoO <jinwoo2511@naver.com>
2024-07-25 03:18:26 +0900

    refac : Mushroom 몬스터 팩토리 기능 수정 및 변수 값 수정
    Mushroom 몬스터 팩토리 기능 수정 및 변수 값 수정

--- CookApps_LeeJinWoo/Assets/1.Scripts/Factory/MushroomFactory.cs
+++ CookApps_LeeJinWoo/Assets/1.Scripts/Factory/MushroomFactory.cs
@@ -4,30 +4,29 @@ using UnityEngine;
 
 public class MushroomFactory : MonoBehaviour, IMonsterFactory
 {
-    public UnitData _monsterPrefab { get; set; }
-    public Stack<UnitData> _monsters { get; set; }
+    [field: SerializeField]
+    public MonsterUnitData MonsterPrefab { get; set; }
 
-    [SerializeField]
-    private UnitData thisMonsterPrefab;
+    // 오브젝트 풀링을 위한 몬스터 유닛 풀
+    public Stack<MonsterUnitData> Monsters { get; set; }
 
     private void Awake()
     {
-
-        // 인터페이스 변수에 값 할당
-        _monsterPrefab = thisMonsterPrefab;
-
         // 스택 초기화
-        _monsters = new Stack<UnitData>();
+        Monsters = new Stack<MonsterUnitData>();
+
+        MonsterPrefab.CharacterId = CharacterNumber.Mushoroom;
+        MonsterPrefab.CharacterType = CharacterType.Monster;
 
         // 오브젝트 풀링 셋팅하는 함수
-        InitObjPool(_monsterPrefab);
+        InitObjPool(MonsterPrefab);
     }
 
     #region # InitObjPool : 오브젝트 풀링 구현해주는 함수
-    public void InitObjPool(UnitData monsterPref)
+    public void InitObjPool(MonsterUnitData monsterPref)
     {
         {
-            UnitData monsterUnit = null;
+            MonsterUnitData monsterUnit = null;
 
 
             // 오브젝트 풀링을 위해 프리팹 미리 생성
@@ -40,13 +39,13 @@ public class MushroomFactory : MonoBehaviour, IMonsterFactory
                 monsterUnit.gameObject.SetActive(false);
 
                 // 생성된 유닛의 팩토리를 이 팩토리로 지정
-                monsterUnit._factory = this;
+                monsterUnit.Factory = this;
 
                 // 인스펙터 창에서 깔끔하게 보이기 위해 팩토리 자식으로 설정
                 monsterUnit.transform.SetParent(transform);
 
                 // 오브젝트 풀링 스택의 요소로 추가
-                _monsters.Push(monsterUnit);
+                Monsters.Push(monsterUnit);
             }
 
         }
@@ -54,21 +53,21 @@ public class MushroomFactory : MonoBehaviour, IMonsterFactory
     #endregion
 
     // 생산될 유닛을 결정해주는 구상 생산자
-    public UnitData CreateMonsterUnit()
+    public MonsterUnitData CreateMonsterUnit()
     {
-        UnitData MonsterUnit = null;
+        MonsterUnitData MonsterUnit = null;
 
         // 스택 요소가 0보다 클 경우
-        if (_monsters.Count > 0)
+        if (Monsters.Count > 0)
         {
-            MonsterUnit = _monsters.Pop();
+            MonsterUnit = Monsters.Pop();
         }
 
         // 스택 요소가 0 보다 작을 경우 요소 추가
         else
         {
-            MonsterUnit = Instantiate(_monsterPrefab);
-            MonsterUnit._factory = this;
+            MonsterUnit = Instantiate(MonsterPrefab);
+            MonsterUnit.Factory = this;
         }
 
         // 생성된 몬스터 반환
