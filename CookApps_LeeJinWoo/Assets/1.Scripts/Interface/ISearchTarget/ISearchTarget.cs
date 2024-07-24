4f4d2588e22335862a7fc9c44cbd4e31675cdb85
22jinWoO <jinwoo2511@naver.com>
2024-07-25 03:19:33 +0900

    refac : ISearchTarget 인터페이스 적 탐지 확인 유무 변수 추가 및 변수 값 및 네이밍 수정
    ISearchTarget 인터페이스 적 탐지 확인 유무 변수 추가 및 변수 값 및 네이밍 수정

--- CookApps_LeeJinWoo/Assets/1.Scripts/Interface/ISearchTarget/ISearchTarget.cs
+++ CookApps_LeeJinWoo/Assets/1.Scripts/Interface/ISearchTarget/ISearchTarget.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public interface ISearchTarget
 {
-    // 유닛이 타겟으로 할 대상
-    public Transform _targetUnit { get; set; }
+    // 적 탐지 확인
+    public bool IsSearch { get; set; }
 
-    public CapsuleCollider2D _targetColider { get; set; }
+    // 유닛이 타겟으로 할 대상
+    public Transform TargetUnit { get; set; }
+
+    public CapsuleCollider2D TargetColider { get; set; }
 
     // 유닛의 행동 상태
-    public IUnitActState _actStateCs { get; set; }
+    public IUnitActState ActState { get; set; }
 
     // 타겟을 탐지하는 함수 선언
     public void SearchTarget();
